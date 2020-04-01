58ea1fad0c12e0fb67bdc952b71c96d3cf714b22
Tyler Truong <tyler_truong@icloud.com>
2020-03-31 20:49:49 -0400

    Added basic HTTP REST game where you can move a person in a room using arrow keys.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
+/**
+ * This is the backend file written using Firebase functions. It is a web server that runs for 10 milliseconds before
+ * turning back off. It will greatly reduce cost for API calls that are infrequent.
+ */
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 import * as express from "express";
 import * as cors from "cors";
 
+/**
+ * Initialize the firebase API.
+ */
 admin.initializeApp();
 
 // Start writing Firebase Functions
@@ -16,8 +23,12 @@ admin.initializeApp();
  */
 const usersApp = express();
 
+// use CORS to allow request to come from any domain, Allow the API to be called by Single Page Applications.
 usersApp.use(cors({origin: true}));
 
+/**
+ * Get a list of users.
+ */
 usersApp.get("/", (req: any, res: { json: (arg0: any) => void; }, next: (arg0: any) => any) => {
     (async () => {
         const querySnapshot = await admin.firestore().collection("users").get();
@@ -34,6 +45,9 @@ usersApp.get("/", (req: any, res: { json: (arg0: any) => void; }, next: (arg0: a
     })().catch((err) => next(err));
 });
 
+/**
+ * Create a new user.
+ */
 usersApp.post("/", (req: { body: { firstName: any; lastName: any; age: any; }; }, res: any, next: (arg0: any) => any) => {
     (async () => {
         const {firstName, lastName, age} = req.body;
@@ -43,6 +57,9 @@ usersApp.post("/", (req: { body: { firstName: any; lastName: any; age: any; }; }
     })().catch((err) => next(err));
 });
 
+/**
+ * Delete a user.
+ */
 usersApp.delete("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: any) => any) => {
     (async () => {
         const {id} = req.params;
@@ -51,6 +68,7 @@ usersApp.delete("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: 
     })().catch((err) => next(err));
 });
 
+// export the express app as a firebase function.
 export const users = functions.https.onRequest(usersApp);
 
 /**
@@ -62,8 +80,12 @@ export const users = functions.https.onRequest(usersApp);
  */
 const carsApp = express();
 
+// Use CORS to allow any URL to access the API, used to enable Single Page Applications.
 carsApp.use(cors({origin: true}));
 
+/**
+ * Get a list of cars.
+ */
 carsApp.get("/", (req: any, res: { json: (arg0: any) => void; }, next: (arg0: any) => any) => {
     (async () => {
         const querySnapshot = await admin.firestore().collection("cars").get();
@@ -80,6 +102,9 @@ carsApp.get("/", (req: any, res: { json: (arg0: any) => void; }, next: (arg0: an
     })().catch((err) => next(err));
 });
 
+/**
+ * Create a new car.
+ */
 carsApp.post("/", (req: { body: { make: any; model: any; vin: any; }; }, res: any, next: (arg0: any) => any) => {
     (async () => {
         const {make, model, vin} = req.body;
@@ -89,6 +114,9 @@ carsApp.post("/", (req: { body: { make: any; model: any; vin: any; }; }, res: an
     })().catch((err) => next(err));
 });
 
+/**
+ * Delete a car.
+ */
 carsApp.delete("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: any) => any) => {
     (async () => {
         const {id} = req.params;
@@ -97,4 +125,95 @@ carsApp.delete("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: a
     })().catch((err) => next(err));
 });
 
+// export the express app as a firebase function
 export const cars = functions.https.onRequest(carsApp);
+
+/**
+ * Person API
+ * The following code implement CRUD, Create, Read, (Update missing), Destroy for all car objects using firebase functions
+ * to process JSON REST API calls that update a firebase firestore database. The nice feature of using firebase functions
+ * is that you pay per invocation or each time the api is called instead of a constant 24/7 uptime. You can break even
+ * if you use the API for less than 12 hours out of a 24 hour day.
+ */
+
+interface IPerson {
+    id: string;
+    x: number;
+    y: number;
+    shirtColor: string;
+    pantColor: string;
+}
+
+const personsApp = express();
+
+// Use CORS to allow any URL to access the API, used to enable Single Page Applications.
+personsApp.use(cors({origin: true}));
+
+/**
+ * Get a list of persons.
+ */
+personsApp.get("/", (req: any, res: { json: (arg0: any) => void; }, next: (arg0: any) => any) => {
+    (async () => {
+        const querySnapshot = await admin.firestore().collection("persons").get();
+        const personsToReturnAsJson = [];
+
+        for (const documentSnapshot of querySnapshot.docs) {
+            personsToReturnAsJson.push({
+                ...documentSnapshot.data()
+            });
+        }
+
+        res.json(personsToReturnAsJson as any);
+    })().catch((err) => next(err));
+});
+
+/**
+ * Create a new person.
+ */
+personsApp.post("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: any) => any) => {
+    (async () => {
+        const id: string = req.params.id;
+        const person: IPerson = {
+            id,
+            x: 50,
+            y: 150,
+            pantColor: "blue",
+            shirtColor: "grey"
+        };
+        await admin.firestore().collection("persons").doc(id).set(person);
+        res.sendStatus(200);
+    })().catch((err) => next(err));
+});
+
+/**
+ * Update a person.
+ */
+personsApp.put("/:id", (req: { params: { id: any; }; body: any; }, res: any, next: (arg0: any) => any) => {
+    (async () => {
+        const id: string = req.params.id;
+        const person: IPerson = {
+            id,
+            x: 50,
+            y: 150,
+            pantColor: "blue",
+            shirtColor: "grey",
+            ...req.body
+        };
+        await admin.firestore().collection("persons").doc(id).set(person);
+        res.sendStatus(200);
+    })().catch((err) => next(err));
+});
+
+/**
+ * Delete a person.
+ */
+personsApp.delete("/:id", (req: { params: { id: any; }; }, res: any, next: (arg0: any) => any) => {
+    (async () => {
+        const {id} = req.params;
+        await admin.firestore().collection("persons").doc(id).delete();
+        res.sendStatus(200);
+    })().catch((err) => next(err));
+});
+
+// export the express app as a firebase function
+export const persons = functions.https.onRequest(personsApp);
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import axios from "axios";
 
 interface IAppProps {}
 
+interface IPerson {
+    id: string;
+    x: number;
+    y: number;
+    shirtColor: string;
+    pantColor: string;
+}
+
+interface IKeyDownHandler {
+    key: "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight";
+    interval: any;
+}
+
 interface IUser {
     id: number;
     firstName: string;
@@ -26,9 +39,24 @@ interface IAppState {
     make: string;
     model: string;
     vin: string;
+    persons: IPerson[];
+    currentPersonId: string;
+    update: boolean;
 }
 
 class App extends React.Component<IAppProps, IAppState> {
+    /**
+     * The interval containing the game loop.
+     */
+    intervalGameLoop: any = null;
+
+    /**
+     * Set Intervals that move the user across the screen.
+     */
+    keyDownHandlers: IKeyDownHandler[] = [];
+
+    gameRefreshSpeed: number = 2000;
+
     state = {
         users: [],
         cars: [],
@@ -37,13 +65,167 @@ class App extends React.Component<IAppProps, IAppState> {
         age: "",
         make: "",
         model: "",
-        vin: ""
+        vin: "",
+        persons: [],
+        currentPersonId: this.randomPersonId(),
+        update: false
     } as IAppState;
 
     componentDidMount(): void {
         this.fetchUsers();
         this.fetchCars();
+        this.createPerson();
+        this.beginGameLoop();
     }
+
+    componentWillUnmount(): void {
+        this.deletePerson();
+        this.endGameLoop();
+    }
+
+    createPerson = async () => {
+        await axios.post(`https://us-central1-tyler-truong-demos.cloudfunctions.net/persons/${this.state.currentPersonId}`);
+    };
+
+    updatePerson = async () => {
+        const data = this.state.persons.find(person => person.id === this.state.currentPersonId);
+        if (data) {
+            await axios.put(`https://us-central1-tyler-truong-demos.cloudfunctions.net/persons/${this.state.currentPersonId}`, data);
+        }
+    };
+
+    deletePerson = async () => {
+        await axios.delete(`https://us-central1-tyler-truong-demos.cloudfunctions.net/persons/${this.state.currentPersonId}`);
+    };
+
+    beginGameLoop = () => {
+        this.intervalGameLoop = setTimeout(this.gameLoop, this.gameRefreshSpeed);
+        window.addEventListener("keydown", this.handleKeyDown);
+        window.addEventListener("keyup", this.handleKeyUp);
+    };
+
+    endGameLoop = () => {
+        if (this.intervalGameLoop) {
+            clearTimeout(this.intervalGameLoop);
+            this.intervalGameLoop = null;
+        }
+        window.removeEventListener("keydown", this.handleKeyDown);
+        window.removeEventListener("keyup", this.handleKeyUp);
+    };
+
+    gameLoop = async () => {
+        if (this.state.update) {
+            await this.updatePerson();
+            await new Promise((resolve) => {
+                this.setState({update: false}, () => {
+                    resolve();
+                });
+            });
+        }
+
+        const response = await axios.get("https://us-central1-tyler-truong-demos.cloudfunctions.net/persons");
+        if (response && response.data) {
+            const persons = response.data;
+            this.setState({persons});
+        }
+
+        this.intervalGameLoop = setTimeout(this.gameLoop, this.gameRefreshSpeed);
+    };
+
+    isPerson = (person: any): person is IPerson => {
+        return person && typeof person.id === "string" && typeof person.x === "number" && typeof person.y === "number" &&
+            typeof person.shirtColor === "string" && person.pantColor === "string";
+    };
+
+    /**
+     * Handle the movement of the current person across the screen.
+     * @param event
+     */
+    handleKeyDown = (event: KeyboardEvent) => {
+        switch (event.key) {
+            case "ArrowUp": {
+                this.keyDownHandlers.push({
+                    key: "ArrowUp",
+                    interval: setInterval(() => {
+                        const person = this.state.persons.find(person => person.id === this.state.currentPersonId);
+                        if (person) {
+                            person.y -= 1;
+                            this.setState({update: true});
+                        }
+                    }, 100)
+                });
+                break;
+            }
+            case "ArrowDown": {
+                this.keyDownHandlers.push({
+                    key: "ArrowDown",
+                    interval: setInterval(() => {
+                        const person = this.state.persons.find(person => person.id === this.state.currentPersonId);
+                        if (person) {
+                            person.y += 1;
+                            this.setState({update: true});
+                        }
+                    }, 100)
+                });
+                break;
+            }
+            case "ArrowLeft": {
+                this.keyDownHandlers.push({
+                    key: "ArrowLeft",
+                    interval: setInterval(() => {
+                        const person = this.state.persons.find(person => person.id === this.state.currentPersonId);
+                        if (person) {
+                            person.x -= 1;
+                            this.setState({update: true});
+                        }
+                    }, 100)
+                });
+                break;
+            }
+            case "ArrowRight": {
+                this.keyDownHandlers.push({
+                    key: "ArrowRight",
+                    interval: setInterval(() => {
+                        const person = this.state.persons.find(person => person.id === this.state.currentPersonId);
+                        if (person) {
+                            person.x += 1;
+                            this.setState({update: true});
+                        }
+                    }, 100)
+                });
+                break;
+            }
+        }
+    };
+
+    /**
+     * Handle the key up event on the SVG canvas. It cancels the interval that moves the person on the screen.
+     * @param event
+     */
+    handleKeyUp = (event: KeyboardEvent) => {
+        // for all handlers
+        while (true) {
+            // find handler for corresponding key up
+            const index = this.keyDownHandlers.findIndex((handler) => {
+                return handler.key === event.key
+            });
+            // if found
+            if (index >= 0) {
+                // remove using mutable method
+                const handlers = this.keyDownHandlers.splice(index, 1);
+                // clear all set intervals for each handler
+                handlers.forEach(handler => {
+                    if (handler.interval) {
+                        clearInterval(handler.interval);
+                        handler.interval = null;
+                    }
+                });
+            } else {
+                // no more handlers, exit loop
+                break;
+            }
+        }
+    };
 
     fetchUsers = async () => {
         const response = await axios.get("https://us-central1-tyler-truong-demos.cloudfunctions.net/users");
@@ -133,9 +315,58 @@ class App extends React.Component<IAppProps, IAppState> {
         this.setState({[field]: event.target.value} as any);
     };
 
+    randomPersonId() {
+        return new Array(10).fill(0).map(() => Number(Math.floor(Math.random() * 36)).toString(36)).join("");
+    }
+
+    drawPerson = (person: IPerson) => {
+        const {x, y} = person;
+        return (
+            <g key={person.id} transform={`translate(${x - 50},${y - 100})`}>
+                <polygon fill="brown" points="40,10 60,10 60,30 40,30"/>
+                <polygon fill="brown" points="20,30 80,30 80,100 20,100"/>
+                <polygon fill="blue" points="20,100 80,100 80,120 20,120"/>
+                <polygon fill="blue" points="20,120 40,120 40,200 20,200"/>
+                <polygon fill="blue" points="80,120 60,120 60,200 80,200"/>
+            </g>
+        );
+    };
+
+    drawTable = (x: number, y: number) => {
+        return (
+            <g transform={`translate(${x - 100},${y - 50})`}>
+                <polygon fill="brown" points="0,100 200,100 200,0 0,0"/>
+            </g>
+        );
+    };
+
+    drawChair = (x: number, y: number) => {
+        return (
+            <g transform={`translate(${x - 50},${y - 50})`}>
+                <polygon fill="brown" points="10,90 20,90 20,10 10,10"/>
+                <polygon fill="brown" points="80,90 90,90 90,10 80,10"/>
+                <polygon fill="brown" points="40,90 60,90 60,10 40,10"/>
+                <polygon fill="brown" points="10,10 90,10 90,20 10,20"/>
+                <polygon fill="brown" points="10,50 90,50 90,90 10,90"/>
+            </g>
+        );
+    };
+
     render() {
         return (
             <div>
+                <svg className="game" width={500} height={300} style={{border: "1px solid black"}}>
+                    {this.drawChair(200, 50)}
+                    {this.drawChair(300, 50)}
+                    {this.drawChair(200, 250)}
+                    {this.drawChair(300, 250)}
+                    {
+                        this.state.persons.map(person => {
+                            return this.drawPerson(person);
+                        })
+                    }
+                    {this.drawTable(250, 150)}
+                </svg>
                 <div className="users">
                     <h1>Users</h1>
                     <div>
