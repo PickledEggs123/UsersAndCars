508bd37e4d3f94d49c03f438fa143f8a9de5d6ac
Tyler Truong <tyler_truong@icloud.com>
2020-04-02 12:00:43 -0400

    Added comments to all of Persons.tsx

--- src/Persons.tsx
+++ src/Persons.tsx
@@ -2,55 +2,166 @@ import React from 'react';
 import './App.scss';
 import axios from "axios";
 
+/**
+ * The input to the [[Persons]] component that changes how the game is rendered.
+ */
 interface IPersonsProps {}
 
+/**
+ * The base interface for all game objects.
+ */
 interface IObject {
+    /**
+     * The left to right position of the object in the game world.
+     */
     x: number;
+    /**
+     * The top to bottom position of the object in the game world.
+     */
     y: number;
 }
 
+/**
+ * The base interface for all people in the game.
+ */
 interface IPerson extends IObject {
+    /**
+     * The randomly generated unique id of the person. Each person has a unique id for selecting and controlling them.
+     */
     id: string;
+    /**
+     * The customizable shirt color of the person.
+     */
     shirtColor: string;
+    /**
+     * The customizable pant color of the person.
+     */
     pantColor: string;
+    /**
+     * When the person was last updated. Used to keep track of which version of the person data is more up to date. The
+     * local copy sometimes can be more up to date than the network copy, so the network copy has to be modified with
+     * local data. If the person moves, they will send their current position to the server. They will continue moving,
+     * making the sent position out of date. The server will confirm the position update then send back the old position.
+     * This field allows the game to reject old copies of position, favoring the newer local position. Without this, the
+     * person will teleport backwards, causing a constant teleport backwards glitch.
+     */
     lastUpdate: string;
 }
 
+/**
+ * The type of [[IDrawable object]].
+ */
 enum EDrawableType {
+    /**
+     * When rendering people, people are drawn on top of objects at the same screen height.
+     */
     PERSON = "PERSON",
+    /**
+     * The [[IDrawable]] is a normal object.
+     */
     OBJECT = "OBJECT"
 }
 
+/**
+ * The state of the doors in a room.
+ */
 interface IRoomDoors {
+    /**
+     * There is a left door.
+     */
     left: boolean;
+    /**
+     * There is a right door.
+     */
     right: boolean;
+    /**
+     * There is a top door.
+     */
     top: boolean;
+    /**
+     * There is a bottom door.
+     */
     bottom: boolean;
 }
 
+/**
+ * A room which contains doors and furniture.
+ */
 interface IRoom extends IObject {
+    /**
+     * The doors of the room.
+     */
     doors: IRoomDoors;
+    /**
+     * The chairs in the room.
+     */
     chairs: IObject[];
+    /**
+     * The tables in the room.
+     */
     tables: IObject[];
 }
 
+/**
+ * An object which can be sorted for rendering a scene. It can be a person, a chair, a table, or walls. Each [[IDrawable]]
+ * is then sorted by height so [[IDrawable]]s on the bottom of the screen will overlap the [[IDrawable]]s above them.
+ * This gives the appearance of a 2D Stereographic Projection using overlapped images.
+ */
 interface IDrawable extends IObject {
+    /**
+     * A function that renders the drawable object.
+     */
     draw(this: IDrawable): void;
+    /**
+     * The type of drawable. [[EDrawableType.PERSON]] and [[EDrawableType.OBJECT]] are sorted differently before drawing.
+     */
     type: EDrawableType;
 }
 
+/**
+ * A key down interval handler. Pressing a key down will begin a setInterval which will run every 100 milliseconds. This
+ * creates a smooth animation of movement. A person will move 10 pixels every 100 milliseconds until the key up event.
+ */
 interface IKeyDownHandler {
+    /**
+     * The key that triggered the handler. Used by the key up handler to cancel [[interval]] then to remove the [[IKeyDownHandler]].
+     */
     key: "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight" | "w" | "a" | "s" | "d";
+    /**
+     * The setTimeout interval which performs the movement or action at a steady rate.
+     */
     interval: any;
 }
 
+/**
+ * The state of the game component. The game state is stored in React so all changes to the game state will update the
+ * SVG on the screen.
+ */
 interface IPersonsState {
+    /**
+     * A list of persons from the network.
+     */
     persons: IPerson[];
+    /**
+     * A list of rooms in the current building.
+     */
     rooms: IRoom[];
+    /**
+     * The randomly generated ID of the current person shown.
+     */
     currentPersonId: string;
+    /**
+     * The timestamp of the last network update. Used to prevent the backward teleporting glitch when moving. If the
+     * local update is newer than the last network update, the local update will replace the network update. This
+     * allows the player to move smoothly across the screen without the network update resetting the player's position
+     * to a previous position.
+     */
     lastUpdate: string;
 }
 
+/**
+ * A React Component which renders the Persons game.
+ */
 export class Persons extends React.Component<IPersonsProps, IPersonsState> {
     /**
      * The interval containing the game loop.
@@ -62,8 +173,16 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
      */
     keyDownHandlers: IKeyDownHandler[] = [];
 
+    /**
+     * The game refresh rate. 1000 means 1 second. Decreasing this value will increase how fluid network movement will
+     * appear. A low value will also make more HTTP REST API JSON calls. The backend uses firebase functions which charges
+     * money per API call.
+     */
     gameRefreshSpeed: number = 2000;
 
+    /**
+     * The state of the game.
+     */
     state = {
         persons: [] as IPerson[],
         rooms: [{
@@ -118,10 +237,16 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         lastUpdate: new Date().toISOString()
     };
 
+    /**
+     * Setup the game.
+     */
     componentDidMount(): void {
         this.beginGameLoop();
     }
 
+    /**
+     * Stop the game.
+     */
     componentWillUnmount(): void {
         this.endGameLoop();
     }
@@ -137,9 +262,11 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
      * Update the person in the database then update the game.
      */
     updatePerson = async () => {
-        const data = this.state.persons.find(person => person.id === this.state.currentPersonId);
-        if (data) {
-            await axios.put(`https://us-central1-tyler-truong-demos.cloudfunctions.net/persons/${this.state.currentPersonId}`, data);
+        const currentPerson = this.state.persons.find(person => person.id === this.state.currentPersonId);
+        if (currentPerson) {
+            // person exist, update the database with the current copy of current person.
+            await axios.put(`https://us-central1-tyler-truong-demos.cloudfunctions.net/persons/${this.state.currentPersonId}`, currentPerson);
+            // wait for [[state.lastUpdate]] to update after the network call.
             await new Promise((resolve) => {
                 this.setState({
                     lastUpdate: new Date().toISOString()
@@ -395,6 +522,9 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         }));
     };
 
+    /**
+     * Generate a random Person Id to control a specific person on the server.
+     */
     randomPersonId() {
         return new Array(10).fill(0).map(() => Number(Math.floor(Math.random() * 36)).toString(36)).join("");
     }
@@ -408,11 +538,19 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
             position.y >= room.y && position.y <= room.y + 300;
     };
 
+    /**
+     * Draw a person as some SVG elements.
+     * @param person The person to draw.
+     */
     drawPerson = (person: IPerson) => {
         const {x, y} = person;
+
+        // the mask property which will mask the person's body so the bottom half of the person does not appear below a wall
         let mask: string = "";
+        // find which room the person is in
         const roomIndex = this.state.rooms.findIndex(this.isInRoom(person));
         if (roomIndex >= 0) {
+            // person is in a room, apply the room mask
             mask = `url(#room-${roomIndex})`;
         }
 
@@ -429,6 +567,12 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         );
     };
 
+    /**
+     * Draw a table in a room.
+     * @param drawable The table to draw.
+     * @param room The room the table is in.
+     * @param index The index of the table.
+     */
     drawTable = (drawable: IObject, room: IObject, index: number) => {
         const {x, y} = drawable;
         return (
@@ -438,6 +582,12 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         );
     };
 
+    /**
+     * Draw a chair in a room.
+     * @param drawable The chair to draw.
+     * @param room The room that contains the chair.
+     * @param index The index of the chair in the room.
+     */
     drawChair = (drawable: IObject, room: IObject, index: number) => {
         const {x, y} = drawable;
         return (
@@ -451,6 +601,11 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         );
     };
 
+    /**
+     * Draw walls around the room.
+     * @param drawable The room to draw walls for.
+     * @param index The index of the room.
+     */
     drawRoomWalls = (drawable: IObject, index: number) => {
         const {x, y} = drawable;
         return [{
@@ -459,6 +614,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
             type: EDrawableType.OBJECT,
             draw(this: IDrawable) {
                 if ((drawable as IRoom).doors.top) {
+                    // there is a top door, draw a wall with a top door
                     return (
                         <g key={`room-${index}-wall-top`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,0 200,0 200,5 0,5"/>
@@ -466,6 +622,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                         </g>
                     );
                 } else {
+                    // there is no top door, draw a plain wall
                     return (
                         <g key={`room-${index}-wall-top`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,0 500,0 500,5 0,5"/>
@@ -479,6 +636,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
             type: EDrawableType.OBJECT,
             draw(this: IDrawable) {
                 if ((drawable as IRoom).doors.bottom) {
+                    // there is a bottom door, draw a wall with a bottom door
                     return (
                         <g key={`room-${index}-wall-top`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,295 200,295 200,300 0,300"/>
@@ -486,6 +644,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                         </g>
                     );
                 } else {
+                    // there is no bottom door, draw a plain wall
                     return (
                         <g key={`room-${index}-wall-bottom`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,295 500,295 500,300 0,300"/>
@@ -499,6 +658,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
             type: EDrawableType.OBJECT,
             draw(this: IDrawable) {
                 if ((drawable as IRoom).doors.left) {
+                    // there is a left door, draw a wall with a left door
                     return (
                         <g key={`room-${index}-wall-top`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,0 5,0 5,100 0,100"/>
@@ -506,6 +666,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                         </g>
                     );
                 } else {
+                    // there is no left door, draw a plain wall
                     return (
                         <g key={`room-${index}-wall-left`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="0,0 5,0 5,300 0,300"/>
@@ -519,6 +680,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
             type: EDrawableType.OBJECT,
             draw(this: IDrawable) {
                 if ((drawable as IRoom).doors.right) {
+                    // there is a right wall, draw a door with a right wall
                     return (
                         <g key={`room-${index}-wall-top`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="495,0 500,0 500,100 495,100"/>
@@ -526,6 +688,7 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                         </g>
                     );
                 } else {
+                    // there is no right wall, draw a plain wall
                     return (
                         <g key={`room-${index}-wall-right`} transform={`translate(${x},${y})`}>
                             <polygon fill="brown" points="495,0 500,0 500,300 495,300"/>
@@ -536,9 +699,15 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
         } as IDrawable];
     };
 
+    /**
+     * Create a sorted list of all drawable objects for final rendering. Objects at the bottom should overlap objects
+     * above them to create a 2D Stereographic Projection, like a 2D with 3D movement arcade game. Sort [[IDrawable]]s
+     * from top to bottom so bottom is drawn last, on top of the [[IDrawable]] above it.
+     */
     sortDrawables = () => {
         const component = this;
         const drawables = [
+            // add all persons
             ...this.state.persons.map(person => ({
                 draw(this: IDrawable) {
                     return component.drawPerson(this as unknown as IPerson);
@@ -546,9 +715,13 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                 type: EDrawableType.PERSON,
                 ...person
             }) as IDrawable),
+
+            // for each room
             ...this.state.rooms.reduce((arr: IDrawable[], room: IRoom): IDrawable[] => {
                 return [
                     ...arr,
+
+                    // add all chairs
                     ...room.chairs.map((chair, index) => ({
                         draw(this: IObject) {
                             return component.drawChair(this, room, index);
@@ -558,9 +731,13 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                     }) as IDrawable)
                 ];
             }, []),
+
+            // for each room
             ...this.state.rooms.reduce((arr: IDrawable[], room: IRoom): IDrawable[] => {
                 return [
                     ...arr,
+
+                    // add all tables
                     ...room.tables.map((table, index) => ({
                         draw(this: IObject) {
                             return component.drawTable(this, room, index);
@@ -570,36 +747,51 @@ export class Persons extends React.Component<IPersonsProps, IPersonsState> {
                     }) as IDrawable)
                 ];
             }, []),
+
+            // for each room
             ...this.state.rooms.reduce((arr: IDrawable[], room: IRoom, index: number): IDrawable[] => {
                 return [
                     ...arr,
+
+                    // add all walls
                     ...component.drawRoomWalls(room, index)
                 ];
             }, [])
         ];
 
-        // sort drawable objects from bottom to top
+        // sort drawable objects from top to bottom
         return drawables.sort((a, b) => {
+            // by default, sort by height difference
             const heightDifference = a.y - b.y;
 
             if (a.type === EDrawableType.PERSON && b.type !== EDrawableType.PERSON && heightDifference > 30) {
+                // person has priority over regular objects
                 return 1;
             } else if (b.type === EDrawableType.PERSON && a.type !== EDrawableType.PERSON && heightDifference < 30) {
+                // person has priority over regular objects
                 return -1;
             } else {
+                // sort by height differences
                 return heightDifference;
             }
         });
     };
 
     render() {
+        // find the current person
         const currentPerson = this.state.persons.find(person => person.id === this.state.currentPersonId);
+
+        // the offset of the entire world
         let worldOffsetX: number = 0;
         let worldOffsetY: number = 0;
+
+        // if current person exist
         if (currentPerson) {
+            // center world around current person, the view should be centered on the person
             worldOffsetX = currentPerson.x - 250;
             worldOffsetY = currentPerson.y - 150;
         }
+
         return (
             <div className="persons">
                 <h1>Multiplayer Room</h1>
